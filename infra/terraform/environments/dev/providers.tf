b9a274537d10ef1035b04612100187107a6e3a8c
Sameer Thapa <s.thapa222258@gmail.com>
2026-03-07 17:52:41 -0600

    PU-18 ci/cd fx

--- infra/terraform/environments/dev/providers.tf
+++ infra/terraform/environments/dev/providers.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "gcs" {
+    bucket = "park-u-terraform-state"
+    prefix = "envs/dev"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
